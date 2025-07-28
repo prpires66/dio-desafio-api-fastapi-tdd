9e4edf6a563600fa6ab5cf016004e9947b1b5fea
Paulo Pires <prpires66@gmail.com>
2025-07-28 20:29:42 -0300

    feat: partial development

--- store/schemas/product.py
+++ store/schemas/product.py
@@ -1,10 +1,37 @@
-from typing import Annotated
-from pydantic import Field
-from store.schemas.base import BaseSchemaMixin
+from decimal import Decimal
+from typing import Annotated, Optional
+from bson import Decimal128
+from pydantic import AfterValidator, Field
+from store.schemas.base import BaseSchemaMixin, OutSchema
 
 
-class ProductIn(BaseSchemaMixin):
-    name: Annotated[str, Field(description="Name of the product")]
-    quantity: Annotated[int, Field(description="Quantity of the product")]
-    price: Annotated[float, Field(description="Price of the product")]
-    status: Annotated[bool, Field(description="Status of the product")]
+class ProductBase(BaseSchemaMixin):
+    name: str = Field(..., description="Product name")
+    quantity: int = Field(..., description="Product quantity")
+    price: Decimal = Field(..., description="Product price")
+    status: bool = Field(..., description="Product status")
+
+
+class ProductIn(ProductBase, BaseSchemaMixin):
+    ...
+
+
+class ProductOut(ProductIn, OutSchema):
+    ...
+
+
+def convert_decimal_128(v):
+    return Decimal128(str(v))
+
+
+Decimal_ = Annotated[Decimal, AfterValidator(convert_decimal_128)]
+
+
+class ProductUpdate(BaseSchemaMixin):
+    quantity: Optional[int] = Field(None, description="Product quantity")
+    price: Optional[Decimal_] = Field(None, description="Product price")
+    status: Optional[bool] = Field(None, description="Product status")
+
+
+class ProductUpdateOut(ProductOut):
+    ...
--- tests/schemas/factories.py
+++
@@ -1,7 +0,0 @@
-def product_data_factory():
-    return {
-        "name": "Iphone 14 pro Max",
-        "quantity": 10,
-        "price": 9999.99,
-        "status": True,
-    }
